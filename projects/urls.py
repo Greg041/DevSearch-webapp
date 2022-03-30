00cb2856036f4fd339837295eb45b5445e890d4a
unknown <gregoriazuaje04@gmail.com>
2022-03-30 11:43:49 -0400

    added styles from theme for projects, single project and create project form html's, created app to handle users profiles in website along with its respective model, added fields in projects app models to complete information needed

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import projects, single_project
+from .views import projects, single_project, create_project, update_project, delete_project
 
 urlpatterns = [
     path('', projects, name="projects"),
-    path('projects/<int:pk>', single_project, name="single_project")
+    path('projects/<int:pk>', single_project, name="single_project"),
+    path('create-project/', create_project, name="create_project"),
+    path('update-project/<int:pk>/', update_project, name="update_project"),
+    path('delete-project/<int:pk>', delete_project, name="delete_project")
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,51 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Project, Review
+from .forms import ProjectForm
 # Create your views here.
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    projects = Project.objects.all()
+    return render(request, 'projects/projects.html', {
+        'projects': projects,
+    })
 
 
-def single_project(request):
-    return render(request, 'projects/single-project.html')
+def single_project(request, pk):
+    project = Project.objects.filter(id=pk).first()
+    return render(request, 'projects/single-project.html', {
+        'project': project,
+    })
+
+
+def create_project(request):
+    form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('projects ')
+    context = {'form': form}
+    return render(request, 'projects/project-form.html', context)
+
+
+def update_project(request, pk):
+    project = Project.objects.filter(id=pk).first()
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form': form}
+    return render(request, 'projects/project-form.html', context)
+
+
+def delete_project(request, pk):
+    project = Project.objects.filter(id=pk).first()
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {
+        'object': project
+    }
+    return render(request, 'projects/delete-template.html', context)
