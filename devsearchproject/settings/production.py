07cacf93a1ae216106a479552525fc1c0ac65f4f
unknown <gregoriazuaje04@gmail.com>
2022-04-08 15:32:33 -0400

    configured email smtp variables to production

--- devsearchproject/settings/production.py
+++ devsearchproject/settings/production.py
@@ -14,7 +14,13 @@ CLOUDINARY_STORAGE = {
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-
 MEDIA_URL = '/devsearch-images/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'devsearchproject@gmail.com'
+EMAIL_HOST_PASSWORD = 'zqewufelhmqijbfc'
 
 django_heroku.settings(locals())
